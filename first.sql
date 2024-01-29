4102c9fe7f641515a881e59045079fb7d28354b9
inz Lukasz Byszewski <gdx153483@wsb.gda.pl>
2024-01-29 12:57:17 +0100

    Update

---
+++ first.sql
@@ -0,0 +1,22 @@
+CREATE TABLE first (
+    id INT AUTO_INCREMENT PRIMARY KEY,  
+    field_one VARCHAR(250), 
+    field_two DATE, 
+    field_tree DATETIME,
+    .
+    .
+    .
+    .
+    field_infinite CHAR(160) 
+
+);
+
+
+
+
+
+
+
+
+
+
---
+++ queries.sql
@@ -0,0 +1,29 @@
+
+
+SELECT * FROM table_name;
+
+
+SELECT imie FROM table_name;
+
+
+SELECT imie, nazwisko  FROM table_name;
+
+
+SELECT imie FROM table_name
+WHERE imie="Adam";
+
+
+SELECT age FROM table_name 
+WHERE age BETWEEN age>10 AND age<=18
+
+SELECT age FROM table_name 
+WHERE age NOT BETWEEN age>10 AND age<=18
+
+
+SELECT imie FROM table_name
+ORDER BY imie ASC, nazwisko DESC;
+
+
+INSERT INTO table_name(((pcolumn) - DELETE is A_I), column_one, column_two, .... column_infinite) VALUES ('$ID(MUST REMOVE - A_I))','$FIRST','$SECOND', ... '$UNKNOWN');
+
+
